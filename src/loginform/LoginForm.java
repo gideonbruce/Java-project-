1c148938333d567062c2b83a2be895ae0b993e50
gideonotieno <gideonotieno99@gmail.com>
2024-10-05 00:04:34 +0300

    using anonymous classes to solve to solve action listener

--- src/loginform/LoginForm.java
+++ src/loginform/LoginForm.java
@@ -62,13 +62,19 @@ public class LoginForm implements ActionListener {
         //confirm password label
         confirmPassLabel = new JLabel("Confirm Password:");
         confirmPassLabel.setBounds(10, 80, 150, 25);
+        panel.add(confirmPassLabel);
+        confirmPassText = new JPasswordField(20);
+        confirmPassText.setBounds(150, 80, 165, 25);
         panel.add(confirmPassText);
+        confirmPassLabel.setVisible(false);
+        confirmPassText.setVisible(false);
         
         //initially hidden
         confirmPassLabel.setVisible(false);
         confirmPassText.setVisible(false);
         
         //login button
+        loginButton.addActionListener(this); //form as listener for each constructor
         loginButton = new JButton("Login");
         loginButton.setBounds(10, 120, 80, 25);
         panel.add(loginButton);
@@ -78,10 +84,13 @@ public class LoginForm implements ActionListener {
         signupButton.setBounds(100, 120, 80, 25);
         signupButton.setVisible(false); //hide initially
         panel.add(signupButton);
+        signupButton.addActionListener(this);
         
         //changing the button between login and signup interfaces
         toggleButton = new JButton("Switch to Signup");
         toggleButton.setBounds(190, 120, 160, 25);
+        //action listennr
+        toggleButton.addActionListener(this);
         panel.add(toggleButton);
         
         //showing interface
@@ -89,7 +98,8 @@ public class LoginForm implements ActionListener {
     }
     
     @Override
-    public void actionPerfomed(ActionEvent e) {
+    public void actionPerfomed(java.awt.event.ActionEvent e) {
+        //event halndling
         if (e.getSource() == toggleButton) {
             if (isLoginMode) {
                 // switching to signup mode
@@ -108,11 +118,13 @@ public class LoginForm implements ActionListener {
                 toggleButton.setText("Switch to Signup");
                 isLoginMode = true;
             }
+            
         } else if (e.getSource() == loginButton) {
             //login handler
             String username = userText.getText();
             String password = new String(passText.getPassword());
             JOptionPane.showMessageDialog(frame, "Login Attempted for user: "+ username);
+
         } else if (e.getSource() == signupButton) {
             String username = userText.getText();
             String password = new String(passText.getPassword());
@@ -125,30 +137,6 @@ public class LoginForm implements ActionListener {
             }
         }
         
-        //toggle between modes on button click
-       /* toggleButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerfomed(java.awt.event.ActionEvent e) {
-                if (isLoginMode) {
-                    // switching to signup mode
-                    confirmPassLabel.setVisible(true);
-                    confirmPassText.setVisible(true);
-                    loginButton.setVisible(false);
-                    signupButton.setVisible(true);
-                    toggleButton.setText("Switch to Login");
-                    isLoginMode = false;
-                } else {
-                    // switching to Login mode
-                    confirmPassLabel.setVisible(false);
-                    confirmPassText.setVisible(false);
-                    loginButton.setVisible(true);
-                    signupButton.setVisible(false);
-                    toggleButton.setText("Switch to Signup");
-                    isLoginMode = true;
-                }
-            }
-        });
-        */
     }
     
     public static void main(String[] args) {
